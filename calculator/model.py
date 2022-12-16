b015da5ba098c35c3b65bfad3413c1564f2ad672
Pablo Muralles <pablomuralles28@gmail.com>
2022-12-15 18:03:58 -0600

    finish the logic of the calculator

--- calculator/model.py
+++ calculator/model.py
@@ -36,7 +36,12 @@ class Model:
             pass
         
         elif caption == '=':
-            self.value = str(self._evaluate())
+            value = self._evaluate()
+            
+            if '.0' in str(value):
+                value = int(value)
+                
+            self.value = str(value)
         
         elif caption == '.':
             if not caption in self.value:
